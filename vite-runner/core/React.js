c959eaccd47389f9eeae40c6015b047002299a3f
MerlinAlex <merlinalex@qq.com>
2024-03-24 22:05:47 +0800

    feat: fiber

--- vite-runner/core/React.js
+++ vite-runner/core/React.js
@@ -21,21 +21,106 @@ const createElement = (type, props, ...children) => {
 }
 
 function render(el, container) {
-  const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(el.type)
+  nextWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [el]
+    }
+  }
+  // const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(el.type)
 
-  Object.keys(el.props).forEach((key) => {
+  // Object.keys(el.props).forEach((key) => {
+  //   if (key !== 'children') {
+  //     dom[key] = el.props[key]
+  //   }
+  // })
+
+  // const children = el.props.children
+  // children.forEach((child) => {
+  //   render(child, dom)
+  // })
+  
+  // container.append(dom)
+}
+
+let nextWorkOfUnit = null
+
+function workloop(deadline) {
+  let shouldYield = false
+  while (!shouldYield && nextWorkOfUnit) {
+    nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit)
+
+
+    shouldYield = deadline.timeRemaining() < 1
+  }
+
+  requestIdleCallback(workloop)
+}
+
+
+function createDom (type) {
+  return type === 'TEXT_ELEMENT'
+    ? document.createTextNode('')
+    : document.createElement(type)
+}
+
+function updateProps (dom, props) {
+  Object.keys(props).forEach((key) => {
     if (key !== 'children') {
-      dom[key] = el.props[key]
+      dom[key] = props[key]
     }
   })
-
-  const children = el.props.children
-  children.forEach((child) => {
-    render(child, dom)
-  })
-  
-  container.append(dom)
 }
+
+function initChildren (fiber) {
+  const children = fiber.props.children
+  let prevChild = null
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevChild.sibling = newFiber
+    }
+    prevChild = newFiber
+  })
+}
+
+function performWorkOfUnit (fiber) {
+  if (!fiber.dom) {
+    // 1. create dom  
+    const dom = (fiber.dom = createDom(fiber.type))
+  
+    fiber.parent.dom.append(dom)
+  
+    // 2. props
+    updateProps(dom, fiber.props)
+  }
+
+
+  // 3. create linked list
+  initChildren(fiber)
+  
+  // 4. return next task
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  if (fiber.sibling) {
+    return fiber.sibling
+  }
+
+  return fiber.parent?.sibling
+}
+
+requestIdleCallback(workloop)
 
 const React = {
   render,
